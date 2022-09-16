08a241921b0da4060e24a215a26b826abac25546
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2022-09-15 17:06:09 -0700

    more incomplete work on getting S3 image upload working

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useRef, useState } from 'react';
-import axios from 'axios';
+import React, { useRef, useState } from "react";
+import axios from "axios";
+import Upload from "./Upload.js";
 
 function App() {
-  const fileInputRef=useRef();
+  const fileInputRef = useRef();
 
   const [file, setFile] = useState();
   function handleChange(event) {
-    setFile(event.target.files[0])
+    setFile(event.target.files[0]);
   }
   function handleSubmit(event) {
-    event.preventDefault()
-    const url = 'http://localhost:3000/uploadFile';
+    event.preventDefault();
+    const url = "http://localhost:3000/uploadFile";
     const formData = new FormData();
-    formData.append('file', file);
-    formData.append('fileName', file.name);
+    formData.append("file", file);
+    formData.append("fileName", file.name);
     const config = {
       headers: {
-        'content-type': 'multipart/form-data',
+        "content-type": "multipart/form-data",
       },
     };
     axios.post(url, formData, config).then((response) => {
       console.log(response.data);
     });
-
   }
 
   return (
     <div className="App">
       <h1>Photos For Alice</h1>
       <h2>upload photos</h2>
-      <form>
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Upload your file:
-          <input type="file" id="my_file_input" onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <Upload />
     </div>
   );
 }
