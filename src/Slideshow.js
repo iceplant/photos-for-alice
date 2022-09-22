3ea8007a673763fd857e1a81372b7f2e8c4d3b89
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2022-09-21 17:38:08 -0700

    working photos list

---
+++ src/Slideshow.js
@@ -0,0 +1,108 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./Slideshow.css";
+import {
+  S3Client,
+  ListObjectsCommand,
+  ListObjectsV2Command,
+} from "@aws-sdk/client-s3";
+
+var AWS = require("aws-sdk");
+const REGION = "us-east-1";
+const CREDENTIALS = {
+  accessKeyId: process.env.REACT_APP_ACCESS,
+  secretAccessKey: process.env.REACT_APP_SECRET,
+};
+const s3Client = new S3Client({ region: REGION, credentials: CREDENTIALS });
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+const delay = 2500;
+
+function Slideshow() {
+  const [index, setIndex] = useState(0);
+  const [imageFileNames, setImageFileNames] = useState([]);
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  const Photoslist = () => {
+    useEffect(() => {
+      const GetPhotos = async () => {
+        const bucketParams = {
+          Bucket: process.env.REACT_APP_BUCKET_NAME,
+          // Key: file.name,
+          // Body: file,
+        };
+        const command = new ListObjectsCommand(bucketParams);
+        const response = await s3Client
+          .send(command)
+          .then((data) => {
+            // console.log("data: ", data);
+            // console.log("Contents: ", data.Contents);
+
+            const newImageFileNames = data.Contents.map((item) => item.Key);
+            // console.log("Filenames: ", newImageFileNames);
+            setImageFileNames(newImageFileNames);
+          })
+          .catch((err) => console.error(err));
+        // return response;
+      };
+      GetPhotos();
+    }, []);
+    return (
+      <div>
+        List of photos:
+        {imageFileNames.map((name) => {
+          return <div>{name}</div>;
+        })}
+      </div>
+    );
+  };
+
+  return (
+    <div className="slideshow">
+      <Photoslist />
+
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((backgroundColor, index) => (
+          <div className="slide" key={index} style={{ backgroundColor }}></div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Slideshow;
